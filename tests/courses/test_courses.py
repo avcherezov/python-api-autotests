7b1dcadf8fc4d7a940382c52866a1f3249dec0df
avcherezov <avcherezov@gmail.com>
2026-01-21 15:02:46 +0300

    Правка ошибки в тестах

--- tests/courses/test_courses.py
+++ tests/courses/test_courses.py
@@ -2,6 +2,7 @@ from http import HTTPStatus
 
 from clients.courses.courses_client import CoursesClient
 from clients.courses.courses_schema import CreateCourseRequestSchema, CreateCourseResponseSchema
+from fixtures.users import UserFixture
 from tools.assertions.base import assert_status_code
 from tools.assertions.courses import assert_create_course_response
 from tools.assertions.schema import validate_json_schema
